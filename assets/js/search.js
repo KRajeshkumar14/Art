1cc2176b8c902b7a57b4fbfe260f6261d2e5dfe0
KRajeshkumar14 <KRajeshkumar14@users.noreply.github.com>
2020-07-12 06:45:59 +0000

    deploy: 3b11124a31ef82cefbd2b34f19557027b3c05ea6

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/GraphicDesign/assets/js/search-data.json', true);
+    request.open('GET', '/Art/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
